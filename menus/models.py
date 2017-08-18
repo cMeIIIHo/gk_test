2d9667855bf49b2ea6e437d2c7fb126c74d1c542
cMeIIIHo <cMeIIIHo7@gmail.com>
2017-08-18 08:59:28 +0300

    save method for MenuItem

--- menus/models.py
+++ menus/models.py
@@ -1,13 +1,30 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 from django.core.urlresolvers import NoReverseMatch
+from django.http import Http404
 
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50)
     parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
-    url = models.CharField(max_length=50)
+    explicit_url = models.CharField(max_length=100, blank=True)
+    named_url = models.CharField(max_length=100, blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.named_url:
+            named_url_parts = self.named_url.split()
+            url_name = named_url_parts[0]
+            params = named_url_parts[1:len(named_url_parts)]
+            reversed_explicit_url = reverse(url_name, args=params)
+            if self.explicit_url:
+                if self.explicit_url != reversed_explicit_url:
+                    raise Http404('explicit_url does not match named_url')
+            else:
+                self.explicit_url = reversed_explicit_url
+        super(MenuItem, self).save(*args, **kwargs)
+
+
 
     def __str__(self):
         return self.name
