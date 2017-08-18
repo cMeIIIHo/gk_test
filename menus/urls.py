52415585f2c673a08366b12aff79d537cfed5512
cMeIIIHo <cMeIIIHo7@gmail.com>
2017-08-18 03:53:26 +0300

    sharpened urls, views

--- menus/urls.py
+++ menus/urls.py
@@ -5,10 +5,6 @@ from menus.views import MenuItemView
 app_name = 'menus'
 
 urlpatterns = [
-    url(r'^(?P<intermediate_parameters>.*)/(?P<menu_item>\w+)/$', MenuItemView.as_view(), name='menu_item_view'),
+    url(r'^$', MenuItemView.as_view(), name='menu_item_view'),
+    url(r'^test/$', MenuItemView.as_view(), name='test_url'),
 ]
-
-# urlpatterns = [
-#     url(r'^(\w+/)+$', menu_item, name='menu_item_view'),
-# ]
-
--- menus/views.py
+++ menus/views.py
@@ -4,10 +4,5 @@ from django.views.generic import View
 
 class MenuItemView(View):
 
-    def get(self, request, intermediate_parameters, menu_item):
-        context = {
-            'intermediate_parameters': intermediate_parameters,
-            'menu_item': menu_item,
-        }
-        return render(request, 'menus/menus.html', context)
-
+    def get(self, request):
+        return render(request, 'menus/menus.html')
